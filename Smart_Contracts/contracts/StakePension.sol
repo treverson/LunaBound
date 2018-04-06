147140b76cd1d076f743104f40b37cc19d947280
Andy Tudhope <tuddy0525@gmail.com>
2018-04-06 19:28:22 +0200

    First smart contract and testing suite

---
+++ Smart_Contracts/contracts/StakePension.sol
@@ -0,0 +1,123 @@
+pragma solidity ^0.4.21;
+
+contract StakePension {
+    address public owner;
+    address public manager;
+    
+    uint public recoveryThreshold;
+    address[] public recoverers;
+    mapping(address => address) public proposedNewOwner;
+    
+    struct adhockWithdraw{
+        uint amount;
+        address recipient;    
+    }
+    
+    struct recurringWithdraw{
+        uint amount;
+        address recipient;    
+        uint period;
+        uint lastWithdraw;
+    }
+    
+    adhockWithdraw public pendingAdhocWithdraw = adhockWithdraw(0,0x0);
+    adhockWithdraw public approvedAdhocWithdraw = adhockWithdraw(0,0x0);
+    
+    recurringWithdraw public pendingRecurringWithdraw = recurringWithdraw(0,0x0,0,0);
+    recurringWithdraw public approvedRecurringWithdraw = recurringWithdraw(0,0x0,0,0);
+    
+    function StakePension(address[] _recoverers, uint _recoveryThreshold, address _fundManager) public payable {
+        owner = msg.sender;
+        recoverers = _recoverers;
+        manager = _fundManager;
+        recoveryThreshold = _recoveryThreshold;
+    }
+    
+    modifier isOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    modifier isManager() {
+        require(msg.sender == manager);
+        _;
+    }
+    
+    modifier isRecoverer() {
+        bool recovererFound = false;
+        for (uint i = 0; i < recoverers.length; i ++) {
+            if (msg.sender == recoverers[i]) {
+                recovererFound = true;
+            }
+        }
+        require(recovererFound);
+        _;
+    }
+    
+    function requestAdhocWithdraw(uint  _amount, address _recipient) external isOwner {
+        require(_amount <= address(this).balance);
+        pendingAdhocWithdraw.amount = _amount;
+        pendingAdhocWithdraw.recipient = _recipient;
+    }
+    
+    function requestRecurringWithdraw(uint _amount, address _recipient, uint _period) external isOwner {
+        require(_amount <= address(this).balance);
+        pendingRecurringWithdraw.amount = _amount;
+        pendingRecurringWithdraw.recipient = _recipient;
+        pendingRecurringWithdraw.period = _period;
+    }
+    
+    function approveAdhocWithdraw() external isManager {
+        approvedAdhocWithdraw = pendingAdhocWithdraw;
+        pendingAdhocWithdraw = adhockWithdraw(0,0x0);
+    }
+    
+    function approveRecurringWithdraw() external isManager {
+        approvedRecurringWithdraw = pendingRecurringWithdraw;
+        pendingRecurringWithdraw = recurringWithdraw(0,0x0,0,0);
+    }
+    
+    function executeAdhocWithdraw() external isManager {
+        approvedAdhocWithdraw.recipient.transfer(approvedAdhocWithdraw.amount);
+        //Adhoc only occurs once, so we need to remove the ability to execute another tx
+        approvedAdhocWithdraw = adhockWithdraw(0,0x0); 
+    }
+
+    function executeRecurringWithdraw(uint _time) external isManager {
+        require(_time>approvedRecurringWithdraw.lastWithdraw);
+        approvedRecurringWithdraw.recipient.transfer(approvedRecurringWithdraw.amount);
+        approvedRecurringWithdraw.lastWithdraw = _time;
+    }
+    
+    function proposeNewOwner(address _address) external isRecoverer {
+        proposedNewOwner[msg.sender] = _address;
+    }
+    
+    function transferOwner() external returns(bool){
+        for(uint i = 0; i < recoverers.length - 1; i++) {
+            uint foundCount = 0;
+            for(uint j = i; j<recoverers.length; j++) {
+                if(proposedNewOwner[recoverers[i]]==proposedNewOwner[recoverers[j]] && proposedNewOwner[recoverers[j]] != 0x0) {
+                    foundCount++;
+                    if(foundCount>=recoveryThreshold) {
+                        owner = proposedNewOwner[recoverers[i]];
+                        return true;
+                    }
+                }                
+            }
+        }
+        return false;
+    }
+
+    function contribute() external payable {
+        emit NewContribution(msg.sender, msg.value, address(this));
+    }
+
+    function forgetMe() external isOwner {
+        selfdestruct(manager);
+        emit PensionDeleted(owner, block.number, address(this), address(this).balance);
+    }
+
+    event NewContribution(address _contributor, uint _amount, address _pensionContract);
+    event PensionDeleted(address _owner, uint _blockNumber, address _pensionContract, uint balancePension);
+}
---
+++ Smart_Contracts/test/stakePension.test.js
@@ -0,0 +1,179 @@
+const assertRevert = require('./helpers/assertRevert')
+const BigNumber = web3.BigNumber
+const StakePension = artifacts.require("./StakePension.sol")
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should()
+
+  contract("StakePension", accounts => {
+    const _owner = accounts[0] // only owner can request withdrawls and destruct contract (right to be forgotten)
+    const _recovery1 = accounts[1] // only recovery addresses can propose new owner address in case of lost keys
+    const _recovery2 = accounts[2] 
+    const _recovery3 = accounts[3]
+    const _manager = accounts[4] // only manager can approve & execute withdrawals
+    const _attacker = accounts[5]
+    const _withdrawalAccount = accounts[6]
+      
+    beforeEach(async () => {
+        stakePension = await StakePension.new([_recovery1, _recovery2, _recovery3], 2, _manager, { from: _owner })
+        await stakePension.contribute({from: _owner, value: web3.toWei(1, 'ether')})
+    })
+
+    describe('setup', () => {
+        it('should deploy correctly and set required addresses', async () => {
+            const owner = await stakePension.owner()
+            owner.should.be.equal(_owner)
+            const manager = await stakePension.manager()
+            manager.should.be.equal(_manager)
+            const balanceArray = await web3.fromWei(web3.eth.getBalance(stakePension.address))
+            // The balance comes back as an object and we need to access only 1 part of it
+            const balance = balanceArray.c[0]
+            balance.should.be.equal(1)
+            // When calling public getters for arrays, we need to pass in the index of the address
+            // in the array we want to test for
+            const recovery1 = await stakePension.recoverers(0)
+            recovery1.should.be.equal(_recovery1)
+            const recovery2 = await stakePension.recoverers(1)
+            recovery2.should.be.equal(_recovery2)
+            const recovery3 = await stakePension.recoverers(2)
+            recovery3.should.be.equal(_recovery3)  
+        })
+    })
+
+    describe('requests', () => {
+        it('should allow the owner to request adhoc withdrawals', async () => {
+            const amount = 1
+            await stakePension.requestAdhocWithdraw(amount, _withdrawalAccount, {from: _owner})
+            const pendingObject = await stakePension.pendingAdhocWithdraw()
+            const pendingBalance = pendingObject[0].c[0]
+            pendingBalance.should.be.equal(1)
+            const pendingAddress = pendingObject[1]
+            pendingAddress.should.be.equal(_withdrawalAccount)
+        })
+
+        it('should allow the owner to request recurring withdrawals', async () => {
+            const amount = 1
+            const period = 604800
+            // Using 604800 because that is the number of seconds in a week, for unix timestamps
+            await stakePension.requestRecurringWithdraw(amount, _withdrawalAccount, period, {from: _owner})
+            const pendingObject = await stakePension.pendingRecurringWithdraw()
+            const pendingBalance = pendingObject[0].c[0]
+            pendingBalance.should.be.equal(1)
+            const pendingAddress = pendingObject[1]
+            pendingAddress.should.be.equal(_withdrawalAccount)
+            const pendingPeriod = pendingObject[2].c[0]
+            pendingPeriod.should.be.equal(period)
+        })
+
+        it('should not allow anyone other than the owner to request adhoc withdrawals', async () => {
+            const amount = 1
+            await assertRevert(stakePension.requestAdhocWithdraw(amount, _withdrawalAccount, {from: _attacker}))
+        })
+
+        it('should not allow anyone other than the owner to request recurring withdrawals', async () => {
+            const amount = 1
+            const period = 604800
+            await assertRevert(stakePension.requestRecurringWithdraw(amount, _withdrawalAccount, period, {from: _attacker}))
+        })
+    })
+
+    describe('approve', () => {
+        it('should allow the manager to approve a pending adhoc withdrawal', async () => {
+            const amount = 1
+            await stakePension.requestAdhocWithdraw(amount, _withdrawalAccount, {from: _owner})
+            await stakePension.approveAdhocWithdraw({from: _manager})
+            const approvedObject = await stakePension.approvedAdhocWithdraw()
+            const approvedBalance = approvedObject[0].c[0]
+            approvedBalance.should.be.equal(1)
+            const approvedAddress = approvedObject[1]
+            approvedAddress.should.be.equal(_withdrawalAccount)
+        })
+
+        it('should allow the manager to approve a pending recurring withdrawal', async () => {
+            const amount = 1
+            const period = 604800
+            await stakePension.requestRecurringWithdraw(amount, _withdrawalAccount, period, {from: _owner})
+            await stakePension.approveRecurringWithdraw({from: _manager})
+            const approvedObject = await stakePension.approvedRecurringWithdraw()
+            const approvedBalance = approvedObject[0].c[0]
+            approvedBalance.should.be.equal(1)
+            const approvedAddress = approvedObject[1]
+            approvedAddress.should.be.equal(_withdrawalAccount)
+            const approvedPeriod = approvedObject[2].c[0]
+            approvedPeriod.should.be.equal(period)
+        })
+
+        it('should not allow anyone other than the manager to approve adhoc withdrawals', async () => {
+            const amount = web3.toWei(1, "ether")
+            await stakePension.requestAdhocWithdraw(amount, _withdrawalAccount, {from: _owner})
+            await assertRevert(stakePension.approveAdhocWithdraw({from: _attacker}))
+        })
+
+        it('should not allow anyone other than the manager to approve recurring withdrawals', async () => {
+            const amount = web3.toWei(1, "ether")
+            const period = 604800
+            await stakePension.requestRecurringWithdraw(amount, _withdrawalAccount, period, {from: _owner})
+            await assertRevert(stakePension.approveRecurringWithdraw({from: _attacker}))
+        })
+    })
+    
+    describe('execute', () => {
+        it('should allow the manager to execute an approved adhoc withdrawal', async () => {
+            const balanceBeforeArray = await web3.fromWei(web3.eth.getBalance(_withdrawalAccount))
+            const balanceBefore = balanceBeforeArray.c[0]
+            const amount = web3.toWei(1, "ether")
+            await stakePension.requestAdhocWithdraw(amount, _withdrawalAccount, {from: _owner})
+            await stakePension.approveAdhocWithdraw({from: _manager})
+            await stakePension.executeAdhocWithdraw({from: _manager})
+            const executedObject = await stakePension.approvedAdhocWithdraw()
+            // Should transfer 1 ETH to _withdrawal account
+            const balanceArray = await web3.fromWei(web3.eth.getBalance(_withdrawalAccount))
+            const balance = balanceArray.c[0]
+            balance.should.be.equal(balanceBefore + 1)
+            // Should set the approved balance back to 0
+            const executedBalance = executedObject[0].c[0]
+            executedBalance.should.be.equal(0)
+            const executedAddress = executedObject[1]
+            executedAddress.should.be.equal('0x0000000000000000000000000000000000000000')
+        })
+
+        it('should allow the manager to execute an approved recurring withdrawal', async () => {
+            const balanceBeforeArray = await web3.fromWei(web3.eth.getBalance(_withdrawalAccount))
+            const balanceBefore = balanceBeforeArray.c[0]
+            const amount = web3.toWei(1, "ether")
+            const period = 604800
+            const period2 = 604800*2
+            await stakePension.requestRecurringWithdraw(amount, _withdrawalAccount, period, {from: _owner})
+            await stakePension.approveRecurringWithdraw({from: _manager})
+            await stakePension.executeRecurringWithdraw(period2, {from: _manager})
+            const executedObject = await stakePension.approvedRecurringWithdraw()
+            // Should transfer 1 ETH to _withdrawal account
+            const balanceArray = await web3.fromWei(web3.eth.getBalance(_withdrawalAccount))
+            const balance = balanceArray.c[0]
+            balance.should.be.equal(balanceBefore + 1)
+            // Should set the approved balance back to 0
+            const firstWithdraw = executedObject[2].c[0]
+            firstWithdraw.should.be.equal(period)
+            const lastWithdraw = executedObject[3].c[0]
+            lastWithdraw.should.be.equal(period2)
+        })
+
+        it('should not allow anyone other than the manager to execute an approved adhoc withdrawal', async () => {
+            const amount = web3.toWei(1, "ether")
+            await stakePension.requestAdhocWithdraw(amount, _withdrawalAccount, {from: _owner})
+            await stakePension.approveAdhocWithdraw({from: _manager})
+            await assertRevert(stakePension.executeAdhocWithdraw({from: _attacker}))
+        })
+
+        it('should not allow anyone other than the manager to execute an approved recurring withdrawal', async () => {
+            const amount = web3.toWei(1, "ether")
+            const period = 604800
+            const period2 = 604800*2
+            await stakePension.requestRecurringWithdraw(amount, _withdrawalAccount, period, {from: _owner})
+            await stakePension.approveRecurringWithdraw({from: _manager})
+            await assertRevert(stakePension.executeRecurringWithdraw(period2, {from: _attacker}))
+        })
+    })
+  })
